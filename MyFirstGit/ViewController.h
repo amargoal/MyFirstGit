405040a4b0fd27c073dd361069e0c0267444d7db
amargoal <amargoal1@hotmail.com>
2016-06-04 20:45:30 -0700

    Initial Commit

---
+++ MyFirstGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstGit
+//
+//  Created by Amar Kant Jha on 04/06/16.
+//  Copyright © 2016 Amar Kant Jha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyFirstGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyFirstGit
+//
+//  Created by Amar Kant Jha on 04/06/16.
+//  Copyright © 2016 Amar Kant Jha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
